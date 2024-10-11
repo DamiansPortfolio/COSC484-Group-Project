8d679590342a5a75d8f87d2ec683205553bd577d
Damian Rozycki <drozyck1@students.towson.edu>
2024-10-10 22:52:40 -0400

    Added basic endpoint.

--- backend/server.js
+++ backend/server.js
@@ -1,20 +1,27 @@
-import express from 'express';
-import cors from 'cors';
+import express from "express";
+import cors from "cors";
 
 const app = express();
 const PORT = process.env.PORT || 5001;
 
 // Enable CORS to allow requests from the frontend (localhost:5173)
-app.use(cors({
-    origin: 'http://localhost:5173'  // Allow requests from your frontend
-}));
+app.use(
+  cors({
+    origin: "http://localhost:5173", // Allow requests from your frontend
+  })
+);
 
 app.use(express.json());
 
-// Import routes
-import artistRoutes from './routes/artistRoutes.js';
-app.use('/api/artists', artistRoutes);
+// Routes
+
+app.get("/", (req, res) => {
+  res.send("Welcome to the website connection API");
+});
+
+import artistRoutes from "./routes/artistRoutes.js";
+app.use("/api/artists", artistRoutes);
 
 app.listen(PORT, () => {
-    console.log(`Server running on port ${PORT}`);
+  console.log(`Server running on port ${PORT}`);
 });
