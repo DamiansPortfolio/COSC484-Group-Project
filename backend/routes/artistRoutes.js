c4eac153976a528c1cc9ac5e2b5d12bd048e7453
Damian Rozycki <drozyck1@students.towson.edu>
2024-10-21 10:20:43 -0400

    Updated routes.

--- backend/routes/artistRoutes.js
+++ backend/routes/artistRoutes.js
@@ -1,8 +1,10 @@
-import express from 'express';
-import { getArtists } from '../controllers/artistController.js'; // Include .js extension
+import express from "express";
+import { getArtists, getArtistById } from "../controllers/artistController.js";
 
 const router = express.Router();
 
-router.get('/', getArtists);
+// Routes for fetching artists
+router.get("/", getArtists); // Get all artists or filter/sort
+router.get("/:id", getArtistById); // Get artist by ID
 
 export default router;
