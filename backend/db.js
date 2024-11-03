99a16721adb6f0719bc97f72ecb6bf2245d99716
Damian Rozycki <drozyck1@students.towson.edu>
2024-11-03 12:18:57 -0500

    Updated db.js

--- backend/db.js
+++ backend/db.js
@@ -1,7 +1,7 @@
 import mongoose from "mongoose" // MongoDB ODM for Node.js
 
 // MongoDB connection function
-export const connectToDatabase = async () => {
+const connectToDatabase = async () => {
   try {
     console.log("Attempting to connect to MongoDB...")
     const conn = await mongoose.connect(process.env.MONGODB_URI)
@@ -13,4 +13,4 @@ export const connectToDatabase = async () => {
   }
 }
 
-export { connectToDatabase }
+export default connectToDatabase
